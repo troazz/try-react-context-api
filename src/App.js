272b6f3dabed45ca548f906b32b99acba6c106fd
Puas Citro Aji Windono <puas.citro@gmail.com>
2018-10-10 17:46:20 +0700

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './components/layout/Header'
 import About from './components/pages/About'
 import Contacts from './components/contacts/Contacts'
